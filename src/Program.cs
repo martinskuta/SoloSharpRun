f4498cbbe31baed5daed0df29bb83aa29a1b145f
Martin Škuta <martin.skuta@outlook.com>
2026-02-17 16:20:38 +0100

    Add initial version

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+var processInfo = new ProcessStartInfo
+{
+    FileName = "dotnet",
+    Arguments = string.Join(' ', ["run", ..args]),
+    UseShellExecute = false,
+};
+
+Console.WriteLine($"Running: {processInfo.FileName} {processInfo.Arguments}");
+
+await Process.Start(processInfo)!.WaitForExitAsync();
